09065f48852066595f6e7a415aeddbef1ebd3d31
skmkqw <tkorsakov77@gmail.com>
2024-05-26 22:50:16 +0200

    Fixed navbar item styles, separated it to a new component

--- ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
+++ ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5f574b6bf8dd3b47c7d9f03ec9f090b5900408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac51a83758525706b34f4ed75225d55f65540d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjectPulse.Web/wwwroot/js/app.js
+++ ProjectPulse.Web/wwwroot/js/app.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const showSidebarBtn = document.getElementById('show-sidebar');
     const mainContent = document.getElementById('main-content');
     const projectTabs = document.querySelectorAll('.sidebar-projects-content-item');
-    const optionTabs = document.querySelectorAll('.sidebar-options-content-item');
 
     hideSidebarBtn.addEventListener('click', () => {
         if (window.innerWidth < 768) {
@@ -29,10 +28,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         projectTab.addEventListener('click', handleProjectClick);
     });
 
-    optionTabs.forEach(optionTab => {
-        optionTab.addEventListener('click', handleOptionClick);
-    });
-
     window.addEventListener('resize', () => {
         adjustSidebar();
     });
@@ -50,7 +45,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function handleProjectClick(event) {
-        optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
+        // optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
         projectTabs.forEach(tab => tab.classList.remove('sidebar-projects-content-item-active'));
         if (window.innerWidth < 768) {
             sidebar.classList.add('sidebar-hidden');
@@ -60,25 +55,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
         event.currentTarget.classList.add('sidebar-projects-content-item-active');
     }
-
-    function handleOptionClick(event) {
-        projectTabs.forEach(tab => tab.classList.remove('sidebar-projects-content-item-active'));
-        optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
-        if (window.innerWidth < 768) {
-            sidebar.classList.add('sidebar-hidden');
-            mainContent.classList.add('main-expanded');
-            showSidebarBtn.classList.remove('sidebar-show-hidden');
-            mainContent.classList.remove('main-hidden');
-        }
-        event.currentTarget.classList.add('sidebar-options-content-item-active');
-    }
 });
-
-function login() {
-    var token = "received_jwt_token";
-    localStorage.setItem('jwtToken', token);
-}
-
-function getToken() {
-    return localStorage.getItem('jwtToken');
-}
