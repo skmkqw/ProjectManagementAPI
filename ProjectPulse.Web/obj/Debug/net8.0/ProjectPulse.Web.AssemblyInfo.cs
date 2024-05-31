87375cde28d9995a6857552d04951e47bdb84023
skmkqw <tkorsakov77@gmail.com>
2024-05-31 02:00:37 +0200

    Fixed not-working javascript issue

--- ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
+++ ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20aafb74fa61ccbc1b8daf08d75309653f7a81dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c88c3404dd2fe494c4b312811003afb386cc072")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjectPulse.Web/wwwroot/js/app.js
+++ ProjectPulse.Web/wwwroot/js/app.js
@@ -1,52 +1,63 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+function AdjustSidebar() {
+    console.log("adjust sidebar invoked")
     const sidebar = document.getElementById('sidebar');
-    const hideSidebarBtn = document.getElementById('hide-sidebar');
     const showSidebarBtn = document.getElementById('show-sidebar');
     const mainContent = document.getElementById('main-content');
-    const projectTabs = document.querySelectorAll('.sidebar-projects-content-item');
-
-    hideSidebarBtn.addEventListener('click', () => {
-        if (window.innerWidth < 768) {
-            mainContent.classList.remove('main-hidden');
-        }
+    
+    if (window.innerWidth < 768) {
         sidebar.classList.add('sidebar-hidden');
         mainContent.classList.add('main-expanded');
         showSidebarBtn.classList.remove('sidebar-show-hidden');
-    });
-
-    showSidebarBtn.addEventListener('click', () => {
-        sidebar.classList.remove('sidebar-hidden');
-        if (window.innerWidth < 768) {
-            sidebar.classList.add('sidebar-fullscreen');
-            mainContent.classList.add('main-hidden');
-        }
-        mainContent.classList.remove('main-expanded');
-        showSidebarBtn.classList.add('sidebar-show-hidden');
-    });
-
-    projectTabs.forEach(projectTab => {
-        projectTab.addEventListener('click', handleProjectClick);
-    });
-
-    window.addEventListener('resize', () => {
-        adjustSidebar();
-    });
-
-    window.addEventListener('load', () => {
-        adjustSidebar();
-    });
-
-    function adjustSidebar() {
-        if (window.innerWidth < 768) {
-            sidebar.classList.add('sidebar-hidden');
-            mainContent.classList.add('main-expanded');
-            showSidebarBtn.classList.remove('sidebar-show-hidden');
-        }
     }
+}
 
-    function handleProjectClick(event) {
-        // optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
+function HandleCloseSidebar()
+{
+    console.log("Handle sidebar close invoked");
+    const mainContent = document.getElementById('main-content');
+    if (window.innerWidth < 768) {
+        mainContent.classList.remove('main-hidden');
+    }
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.add('sidebar-hidden');
+    mainContent.classList.add('main-expanded');
+    const showSidebarBtn = document.getElementById('show-sidebar');
+    showSidebarBtn.classList.remove('sidebar-show-hidden');
+}
+
+function HandleOpenSidebar()
+{
+    console.log("Handle sidebar open invoked");
+    const mainContent = document.getElementById('main-content');
+    if (window.innerWidth < 768) {
+        mainContent.classList.add('main-hidden');
+    }
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.remove('sidebar-hidden');
+    mainContent.classList.remove('main-expanded');
+    const showSidebarBtn = document.getElementById('show-sidebar');
+    showSidebarBtn.classList.add('sidebar-show-hidden');
+}
+
+function HandleProjectTabClick()
+{
+    console.log("Handle project tab click invoked");
+    const projectTabs = document.querySelectorAll('.sidebar-projects-content-item');
+    
+    projectTabs.forEach(projectTab => {
+        projectTab.addEventListener('click', ProjectClickEventListener);
+    });
+    
+    function ProjectClickEventListener(event) {
+        
+        const sidebar = document.getElementById('sidebar');
+        const optionTabs = document.querySelectorAll('.sidebar-options-content-item'); 
+        const showSidebarBtn = document.getElementById('show-sidebar');
+        const mainContent = document.getElementById('main-content');
+
+        optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
         projectTabs.forEach(tab => tab.classList.remove('sidebar-projects-content-item-active'));
+        
         if (window.innerWidth < 768) {
             sidebar.classList.add('sidebar-hidden');
             mainContent.classList.add('main-expanded');
@@ -55,4 +66,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
         event.currentTarget.classList.add('sidebar-projects-content-item-active');
     }
-});
+}
+
+function HandleOptionTabClick()
+{
+    console.log("Handle option tab click invoked");
+    const optionTabs = document.querySelectorAll('.sidebar-options-content-item');
+    
+    const projectTabs = document.querySelectorAll('.sidebar-projects-content-item');
+
+    optionTabs.forEach(optionTab => {
+        optionTab.addEventListener('click', OptionClickEventListener);
+    });
+
+    function OptionClickEventListener(event) {
+
+        const sidebar = document.getElementById('sidebar');
+        const optionTabs = document.querySelectorAll('.sidebar-options-content-item');
+        const showSidebarBtn = document.getElementById('show-sidebar');
+        const mainContent = document.getElementById('main-content');
+
+        optionTabs.forEach(tab => tab.classList.remove('sidebar-options-content-item-active'));
+        projectTabs.forEach(tab => tab.classList.remove('sidebar-projects-content-item-active'));
+
+        if (window.innerWidth < 768) {
+            sidebar.classList.add('sidebar-hidden');
+            mainContent.classList.add('main-expanded');
+            showSidebarBtn.classList.remove('sidebar-show-hidden');
+            mainContent.classList.remove('main-hidden');
+        }
+        event.currentTarget.classList.add('sidebar-options-content-item-active');
+    }
+}
