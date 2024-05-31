da828399cce0979983691f969a782e6886dc785a
skmkqw <tkorsakov77@gmail.com>
2024-05-31 03:21:55 +0200

    Basic project page markup and styles

--- ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
+++ ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c88c3404dd2fe494c4b312811003afb386cc072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87375cde28d9995a6857552d04951e47bdb84023")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
