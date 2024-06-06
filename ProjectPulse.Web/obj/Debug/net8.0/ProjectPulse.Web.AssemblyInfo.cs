bf6aa2a43ba20317c72d00784c13813db2c0d925
skmkqw <tkorsakov77@gmail.com>
2024-06-06 20:02:30 +0200

    Merge with frontend tasks

--- ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
+++ ProjectPulse.Web/obj/Debug/net8.0/ProjectPulse.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4079370f993ed49c85c0b947e3975a3cbde68796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ac5f4757381bc184a6a26b545fb56b42a9331b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPulse.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
