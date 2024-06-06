204be3cedd9c6fec56a318023556ad32ddb1ad47
skmkqw <tkorsakov77@gmail.com>
2024-06-06 17:25:57 +0200

    Task card dropdown menu

--- ProjectPulse.Core/obj/Debug/net8.0/ProjectPulse.Core.AssemblyInfo.cs
+++ ProjectPulse.Core/obj/Debug/net8.0/ProjectPulse.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPulse.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d94bfe732635a09cc483717d7a38c697ab25394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3dc2d07c3c511914d91dab9c45c5d2410b0cf22")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPulse.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPulse.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
